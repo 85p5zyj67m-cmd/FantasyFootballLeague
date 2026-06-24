ffe049c0d8e0d0abfef9e0ee0b63900d3511fdf0
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-24 23:16:30 +0200

    config.js aktualisieren

--- js/config.js
+++ js/config.js
@@ -1,1 +1,12 @@
-
+export const GAME_CONFIG = {
+  totalTeams: 20,
+  totalRounds: 16,
+  userTeamIndex: 0,
+  draftPool: {
+    ATT: 120,
+    MID: 120,
+    DEF: 120,
+    GK: 40
+  },
+  visiblePlayersPerPosition: 10
+};
