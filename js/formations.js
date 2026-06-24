9c09e01eba9c90708f59e2dccb484f0b10ed4c4d
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-24 23:22:40 +0200

    formations.js aktualisieren

--- js/formations.js
+++ js/formations.js
@@ -1,1 +1,26 @@
+export const FORMATIONS = [
+  {
+    id: "4-4-2",
+    name: "4-4-2",
+    lines: [["ATT", "ATT"], ["MID", "MID", "MID", "MID"], ["DEF", "DEF", "DEF", "DEF"], ["GK"]]
+  },
+  {
+    id: "4-3-3",
+    name: "4-3-3",
+    lines: [["ATT", "ATT", "ATT"], ["MID", "MID", "MID"], ["DEF", "DEF", "DEF", "DEF"], ["GK"]]
+  },
+  {
+    id: "3-5-2",
+    name: "3-5-2",
+    lines: [["ATT", "ATT"], ["MID", "MID", "MID", "MID", "MID"], ["DEF", "DEF", "DEF"], ["GK"]]
+  },
+  {
+    id: "4-2-3-1",
+    name: "4-2-3-1",
+    lines: [["ATT"], ["MID", "MID", "MID"], ["MID", "MID"], ["DEF", "DEF", "DEF", "DEF"], ["GK"]]
+  }
+];
 
+export function getFormationById(id) {
+  return FORMATIONS.find(f => f.id === id) || FORMATIONS[0];
+}
