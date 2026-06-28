6a8d1a0ec507d6475931dd2a814b95a17660ede9
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-28 17:44:28 +0200

    Refresh router for division draw page

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -1,4 +1,4 @@
-import { goTo } from "./linearRouter.js?v=page-five-team-setup-1";
+import { goTo } from "./linearRouter.js?v=division-draw-1";
 import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
