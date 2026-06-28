9a14f5632f61b00fefbe5494802ae1f9a60e54a2
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-29 01:21:42 +0200

    Install player trait enhancer

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -1,11 +1,13 @@
-import { goTo } from "./linearRouter.js?v=second-half-route-2";
+import { goTo } from "./linearRouter.js?v=player-traits-1";
 import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js?v=default-fast-green-draft-1";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
+import { installPlayerTraitEnhancer } from "./playerTraitEnhancer.js?v=player-traits-1";
 
 window.addEventListener("DOMContentLoaded", () => {
   installLinearStyles();
   installDraftPolishStyles();
   installDraftSpeedController();
+  installPlayerTraitEnhancer();
   goTo("page01");
 });
