dbc1962228e67cf2a71256e80c2b761fd8dfb65e
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-28 17:25:46 +0200

    Add delegated live draft speed controller

---
+++ js/linear/draftSpeedController.js
@@ -0,0 +1,19 @@
+import { appState } from "./linearState.js";
+import { goTo } from "./linearRouter.js?v=draft-speed-live-2";
+
+export function installDraftSpeedController() {
+  document.addEventListener("input", handleSpeedEvent, true);
+  document.addEventListener("change", handleSpeedEvent, true);
+}
+
+function handleSpeedEvent(event) {
+  const select = event.target;
+  if (!(select instanceof HTMLSelectElement)) return;
+  if (!select.closest(".linear-speed-box")) return;
+
+  appState.aiSpeed = select.value;
+
+  window.setTimeout(() => {
+    goTo("page04");
+  }, 0);
+}
