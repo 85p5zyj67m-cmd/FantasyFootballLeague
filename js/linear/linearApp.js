50c3d151be794067652971e10aced438b238aebb
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-28 17:01:54 +0200

    Load draft polish styles

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -1,7 +1,9 @@
 import { goTo } from "./linearRouter.js";
 import { installLinearStyles } from "./linearStyles.js";
+import { installDraftPolishStyles } from "./draftPolishStyles.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   installLinearStyles();
+  installDraftPolishStyles();
   goTo("page01");
 });
