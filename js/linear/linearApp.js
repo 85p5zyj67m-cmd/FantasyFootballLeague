81bf4ba1c7b53960c44c7990672a60c7658d5d4d
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-28 18:25:13 +0200

    Refresh draft polish imports

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -1,6 +1,6 @@
-import { goTo } from "./linearRouter.js?v=compass-target-1";
+import { goTo } from "./linearRouter.js?v=default-fast-green-draft-1";
 import { installLinearStyles } from "./linearStyles.js";
-import { installDraftPolishStyles } from "./draftPolishStyles.js";
+import { installDraftPolishStyles } from "./draftPolishStyles.js?v=default-fast-green-draft-1";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
 
 window.addEventListener("DOMContentLoaded", () => {
