119b8312420b67e7a58cfa580e9ce09de225cbb8
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-29 01:30:33 +0200

    Disable My Team trait observer

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -1,13 +1,11 @@
-import { goTo } from "./linearRouter.js?v=player-traits-1";
+import { goTo } from "./linearRouter.js?v=player-traits-2";
 import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js?v=default-fast-green-draft-1";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
-import { installPlayerTraitEnhancer } from "./playerTraitEnhancer.js?v=player-traits-1";
 
 window.addEventListener("DOMContentLoaded", () => {
   installLinearStyles();
   installDraftPolishStyles();
   installDraftSpeedController();
-  installPlayerTraitEnhancer();
   goTo("page01");
 });
