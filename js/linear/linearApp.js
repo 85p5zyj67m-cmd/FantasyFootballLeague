3d7e5d27a7e5004b8235be1b5f87107f59ba353d
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-28 17:37:49 +0200

    Refresh router for page five setup

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -1,4 +1,4 @@
-import { goTo } from "./linearRouter.js?v=draft-speed-live-3";
+import { goTo } from "./linearRouter.js?v=page-five-team-setup-1";
 import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
