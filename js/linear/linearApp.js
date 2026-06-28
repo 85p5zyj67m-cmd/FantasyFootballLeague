ae0776fbf954e88c474d028218dddc854623d3b1
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-28 17:26:39 +0200

    Install delegated draft speed controller

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -1,9 +1,11 @@
-import { goTo } from "./linearRouter.js?v=draft-speed-live-1";
+import { goTo } from "./linearRouter.js?v=draft-speed-live-2";
 import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js";
+import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-2";
 
 window.addEventListener("DOMContentLoaded", () => {
   installLinearStyles();
   installDraftPolishStyles();
+  installDraftSpeedController();
   goTo("page01");
 });
