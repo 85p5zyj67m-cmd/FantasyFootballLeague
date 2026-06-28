b50b4ead56777d0856ee662d9fbd4cd8f3d6be94
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-28 15:47:45 +0200

    Add linear router

---
+++ js/linear/linearRouter.js
@@ -0,0 +1,54 @@
+import { renderPage01StartDraft } from "./pages/page01StartDraft.js";
+import { renderPage02DraftPosition } from "./pages/page02DraftPosition.js";
+import { renderPage03YourSystem } from "./pages/page03YourSystem.js";
+import { renderPage04Draft } from "./pages/page04Draft.js";
+import { renderPage05Tactics } from "./pages/page05Tactics.js";
+import { renderPage06SeasonStats } from "./pages/page06SeasonStats.js";
+import { renderPage07Match1 } from "./pages/page07Match1.js";
+import { renderPage08Match2 } from "./pages/page08Match2.js";
+import { renderPage09Match3 } from "./pages/page09Match3.js";
+import { renderPage10Match4 } from "./pages/page10Match4.js";
+import { renderPage11FirstHalfOverview } from "./pages/page11FirstHalfOverview.js";
+import { renderPage12Match5 } from "./pages/page12Match5.js";
+import { renderPage13Match6 } from "./pages/page13Match6.js";
+import { renderPage14Match7 } from "./pages/page14Match7.js";
+import { renderPage15Match8 } from "./pages/page15Match8.js";
+import { renderPage16SecondHalfOverview } from "./pages/page16SecondHalfOverview.js";
+import { renderPage17RoundOf16 } from "./pages/page17RoundOf16.js";
+import { renderPage18QuarterFinal } from "./pages/page18QuarterFinal.js";
+import { renderPage19SemiFinal } from "./pages/page19SemiFinal.js";
+import { renderPage20Final } from "./pages/page20Final.js";
+import { renderSeasonEndPage } from "./pages/seasonEndPage.js";
+
+const routes = {
+  page01: renderPage01StartDraft,
+  page02: renderPage02DraftPosition,
+  page03: renderPage03YourSystem,
+  page04: renderPage04Draft,
+  page05: renderPage05Tactics,
+  page06: renderPage06SeasonStats,
+  page07: renderPage07Match1,
+  page08: renderPage08Match2,
+  page09: renderPage09Match3,
+  page10: renderPage10Match4,
+  page11: renderPage11FirstHalfOverview,
+  page12: renderPage12Match5,
+  page13: renderPage13Match6,
+  page14: renderPage14Match7,
+  page15: renderPage15Match8,
+  page16: renderPage16SecondHalfOverview,
+  page17: renderPage17RoundOf16,
+  page18: renderPage18QuarterFinal,
+  page19: renderPage19SemiFinal,
+  page20: renderPage20Final,
+  seasonEnd: renderSeasonEndPage
+};
+
+export function goTo(routeName) {
+  const render = routes[routeName];
+  if (!render) {
+    throw new Error(`Unknown route: ${routeName}`);
+  }
+  render();
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
