723d87b06d3a1b7fe0d429177ca845d785c3ebfe
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-29 16:01:04 +0200

    Bump flexible chain segment UI version

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -3,7 +3,7 @@ import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js?v=default-fast-green-draft-1";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
 import { installFormationSelectorEnhancer } from "./formationSelectorEnhancer.js?v=formation-links-1";
-import { installTraitChainEnhancer } from "./traitChainEnhancer.js?v=chain-roadmap-2";
+import { installTraitChainEnhancer } from "./traitChainEnhancer.js?v=chain-segments-1";
 import { installStrictPositionEnforcer } from "./strictPositionEnforcer.js?v=strict-cdm-1";
 
 window.addEventListener("DOMContentLoaded", () => {
