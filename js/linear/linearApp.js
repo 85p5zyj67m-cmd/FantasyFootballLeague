c60975122078cb7e12b930f96a6448b8724440e5
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-29 03:37:38 +0200

    Install My Team formation selector enhancer

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -2,10 +2,12 @@ import { goTo } from "./linearRouter.js?v=detailed-formations-1";
 import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js?v=default-fast-green-draft-1";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
+import { installFormationSelectorEnhancer } from "./formationSelectorEnhancer.js?v=detailed-formations-2";
 
 window.addEventListener("DOMContentLoaded", () => {
   installLinearStyles();
   installDraftPolishStyles();
   installDraftSpeedController();
+  installFormationSelectorEnhancer();
   goTo("page01");
 });
