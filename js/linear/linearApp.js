2fc65bfa102680dd728e578256243bce543d3c02
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-29 02:52:47 +0200

    Bump router import for compact page 5 cards

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -1,4 +1,4 @@
-import { goTo } from "./linearRouter.js?v=player-traits-2";
+import { goTo } from "./linearRouter.js?v=compact-page5-router-1";
 import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js?v=default-fast-green-draft-1";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
