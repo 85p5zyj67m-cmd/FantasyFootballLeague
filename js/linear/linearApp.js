e6ff9c1dd65a0472ab0f9bc3c151d7a0544ddcc6
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-29 14:49:20 +0200

    Install strict position enforcer and chain box UI

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -3,7 +3,8 @@ import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js?v=default-fast-green-draft-1";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
 import { installFormationSelectorEnhancer } from "./formationSelectorEnhancer.js?v=formation-links-1";
-import { installTraitChainEnhancer } from "./traitChainEnhancer.js?v=formation-links-1";
+import { installTraitChainEnhancer } from "./traitChainEnhancer.js?v=chain-box-1";
+import { installStrictPositionEnforcer } from "./strictPositionEnforcer.js?v=strict-cdm-1";
 
 window.addEventListener("DOMContentLoaded", () => {
   installLinearStyles();
@@ -11,5 +12,6 @@ window.addEventListener("DOMContentLoaded", () => {
   installDraftSpeedController();
   installFormationSelectorEnhancer();
   installTraitChainEnhancer();
+  installStrictPositionEnforcer();
   goTo("page01");
 });
