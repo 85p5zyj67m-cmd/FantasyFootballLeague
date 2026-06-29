8c892edb529e46cc6514c6a97a41ab49574c048e
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-29 04:11:49 +0200

    Install trait chain UI enhancer

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -3,11 +3,13 @@ import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js?v=default-fast-green-draft-1";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
 import { installFormationSelectorEnhancer } from "./formationSelectorEnhancer.js?v=detailed-formations-2";
+import { installTraitChainEnhancer } from "./traitChainEnhancer.js?v=chain-ui-1";
 
 window.addEventListener("DOMContentLoaded", () => {
   installLinearStyles();
   installDraftPolishStyles();
   installDraftSpeedController();
   installFormationSelectorEnhancer();
+  installTraitChainEnhancer();
   goTo("page01");
 });
