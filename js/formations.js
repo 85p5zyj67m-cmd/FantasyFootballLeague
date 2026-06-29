94f341aacc2917d987980664afc95e685149c772
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-29 03:31:33 +0200

    Replace formations with detailed football positions

--- js/formations.js
+++ js/formations.js
@@ -1,23 +1,108 @@
 export const FORMATIONS = [
   {
-    id: "4-4-2",
-    name: "4-4-2",
-    lines: [["ATT", "ATT"], ["MID", "MID", "MID", "MID"], ["DEF", "DEF", "DEF", "DEF"], ["GK"]]
-  },
-  {
     id: "4-3-3",
     name: "4-3-3",
-    lines: [["ATT", "ATT", "ATT"], ["MID", "MID", "MID"], ["DEF", "DEF", "DEF", "DEF"], ["GK"]]
-  },
-  {
-    id: "3-5-2",
-    name: "3-5-2",
-    lines: [["ATT", "ATT"], ["MID", "MID", "MID", "MID", "MID"], ["DEF", "DEF", "DEF"], ["GK"]]
+    lines: [["LW", "ST", "RW"], ["CM", "CDM", "CM"], ["LB", "CB", "CB", "RB"], ["GK"]]
   },
   {
     id: "4-2-3-1",
     name: "4-2-3-1",
-    lines: [["ATT"], ["MID", "MID", "MID"], ["MID", "MID"], ["DEF", "DEF", "DEF", "DEF"], ["GK"]]
+    lines: [["ST"], ["LW", "CAM", "RW"], ["CDM", "CDM"], ["LB", "CB", "CB", "RB"], ["GK"]]
+  },
+  {
+    id: "4-4-2",
+    name: "4-4-2",
+    lines: [["ST", "ST"], ["LM", "CM", "CM", "RM"], ["LB", "CB", "CB", "RB"], ["GK"]]
+  },
+  {
+    id: "4-4-1-1",
+    name: "4-4-1-1",
+    lines: [["ST"], ["CF"], ["LM", "CM", "CM", "RM"], ["LB", "CB", "CB", "RB"], ["GK"]]
+  },
+  {
+    id: "4-1-4-1",
+    name: "4-1-4-1",
+    lines: [["ST"], ["LM", "CM", "CM", "RM"], ["CDM"], ["LB", "CB", "CB", "RB"], ["GK"]]
+  },
+  {
+    id: "4-5-1",
+    name: "4-5-1",
+    lines: [["ST"], ["LM", "CM", "CDM", "CM", "RM"], ["LB", "CB", "CB", "RB"], ["GK"]]
+  },
+  {
+    id: "4-1-2-1-2",
+    name: "4-1-2-1-2 Diamond",
+    lines: [["ST", "ST"], ["CAM"], ["CM", "CM"], ["CDM"], ["LB", "CB", "CB", "RB"], ["GK"]]
+  },
+  {
+    id: "4-3-1-2",
+    name: "4-3-1-2",
+    lines: [["ST", "ST"], ["CAM"], ["CM", "CDM", "CM"], ["LB", "CB", "CB", "RB"], ["GK"]]
+  },
+  {
+    id: "4-3-2-1",
+    name: "4-3-2-1 Christmas Tree",
+    lines: [["ST"], ["CAM", "CAM"], ["CM", "CDM", "CM"], ["LB", "CB", "CB", "RB"], ["GK"]]
+  },
+  {
+    id: "4-2-2-2",
+    name: "4-2-2-2",
+    lines: [["ST", "ST"], ["CAM", "CAM"], ["CDM", "CDM"], ["LB", "CB", "CB", "RB"], ["GK"]]
+  },
+  {
+    id: "4-2-4",
+    name: "4-2-4",
+    lines: [["LW", "ST", "ST", "RW"], ["CM", "CM"], ["LB", "CB", "CB", "RB"], ["GK"]]
+  },
+  {
+    id: "4-1-2-3",
+    name: "4-1-2-3",
+    lines: [["LW", "ST", "RW"], ["CM", "CM"], ["CDM"], ["LB", "CB", "CB", "RB"], ["GK"]]
+  },
+  {
+    id: "3-5-2",
+    name: "3-5-2",
+    lines: [["ST", "ST"], ["LM", "CM", "CDM", "CM", "RM"], ["CB", "CB", "CB"], ["GK"]]
+  },
+  {
+    id: "3-4-3",
+    name: "3-4-3",
+    lines: [["LW", "ST", "RW"], ["LM", "CM", "CM", "RM"], ["CB", "CB", "CB"], ["GK"]]
+  },
+  {
+    id: "3-4-1-2",
+    name: "3-4-1-2",
+    lines: [["ST", "ST"], ["CAM"], ["LM", "CM", "CM", "RM"], ["CB", "CB", "CB"], ["GK"]]
+  },
+  {
+    id: "3-4-2-1",
+    name: "3-4-2-1",
+    lines: [["ST"], ["CAM", "CAM"], ["LM", "CM", "CM", "RM"], ["CB", "CB", "CB"], ["GK"]]
+  },
+  {
+    id: "3-1-4-2",
+    name: "3-1-4-2",
+    lines: [["ST", "ST"], ["LM", "CM", "CM", "RM"], ["CDM"], ["CB", "CB", "CB"], ["GK"]]
+  },
+  {
+    id: "5-3-2",
+    name: "5-3-2",
+    lines: [["ST", "ST"], ["CM", "CDM", "CM"], ["LWB", "CB", "CB", "CB", "RWB"], ["GK"]]
+  },
+  {
+    id: "5-2-3",
+    name: "5-2-3",
+    lines: [["LW", "ST", "RW"], ["CM", "CM"], ["LWB", "CB", "CB", "CB", "RWB"], ["GK"]]
+  },
+  {
+    id: "5-4-1",
+    name: "5-4-1",
+    lines: [["ST"], ["LM", "CM", "CM", "RM"], ["LWB", "CB", "CB", "CB", "RWB"], ["GK"]]
+  },
+  {
+    id: "5-2-1-2",
+    name: "5-2-1-2",
+    lines: [["ST", "ST"], ["CAM"], ["CM", "CM"], ["LWB", "CB", "CB", "CB", "RWB"], ["GK"]]
   }
 ];
 
