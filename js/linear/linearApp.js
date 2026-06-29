66fe03c301778293dc0281d4d29fc5653bdcfbeb
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-29 04:43:55 +0200

    Bump visible selected chain line version

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -3,7 +3,7 @@ import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js?v=default-fast-green-draft-1";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
 import { installFormationSelectorEnhancer } from "./formationSelectorEnhancer.js?v=detailed-formations-2";
-import { installTraitChainEnhancer } from "./traitChainEnhancer.js?v=selected-chain-ui-1";
+import { installTraitChainEnhancer } from "./traitChainEnhancer.js?v=visible-chain-lines-1";
 
 window.addEventListener("DOMContentLoaded", () => {
   installLinearStyles();
