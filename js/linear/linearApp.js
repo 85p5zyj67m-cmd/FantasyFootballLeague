06dbf060fde998d6aaee576e97a135fd689243d0
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-29 14:35:52 +0200

    Bump formation link UI versions

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -2,8 +2,8 @@ import { goTo } from "./linearRouter.js?v=detailed-formations-1";
 import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js?v=default-fast-green-draft-1";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
-import { installFormationSelectorEnhancer } from "./formationSelectorEnhancer.js?v=detailed-formations-2";
-import { installTraitChainEnhancer } from "./traitChainEnhancer.js?v=chain-adjacency-1";
+import { installFormationSelectorEnhancer } from "./formationSelectorEnhancer.js?v=formation-links-1";
+import { installTraitChainEnhancer } from "./traitChainEnhancer.js?v=formation-links-1";
 
 window.addEventListener("DOMContentLoaded", () => {
   installLinearStyles();
