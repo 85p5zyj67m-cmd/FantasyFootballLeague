f2f148911b0358da3df96a2552faae9080aa8fbb
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-30 16:47:57 +0200

    Use position based chain UI enhancer

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -3,7 +3,7 @@ import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js?v=default-fast-green-draft-1";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
 import { installFormationSelectorEnhancer } from "./formationSelectorEnhancer.js?v=formation-links-1";
-import { installTraitChainEnhancer } from "./traitChainEnhancerCompleteV3.js?v=chain-ui-v3-1";
+import { installTraitChainEnhancer } from "./traitChainEnhancerCompleteV4.js?v=position-chain-ui-1";
 import { installTacticsSystemEnhancer } from "./tacticsSystemEnhancer.js";
 import { installStrictPositionEnforcer } from "./strictPositionEnforcer.js?v=strict-cdm-1";
 import { installSeasonStartBridge } from "./seasonStartBridge.js?v=live-balanced-engine-1";
