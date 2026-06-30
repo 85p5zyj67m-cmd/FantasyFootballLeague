52f87b1af020418d3c1ab28efc16e8e61aaed6e2
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-30 15:06:59 +0200

    Bump chain UI import for all-traits coverage

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -3,7 +3,7 @@ import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js?v=default-fast-green-draft-1";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
 import { installFormationSelectorEnhancer } from "./formationSelectorEnhancer.js?v=formation-links-1";
-import { installTraitChainEnhancer } from "./traitChainEnhancer.js?v=positional-chain-recipes-1";
+import { installTraitChainEnhancer } from "./traitChainEnhancer.js?v=all-traits-chain-recipes-1";
 import { installTacticsSystemEnhancer } from "./tacticsSystemEnhancer.js";
 import { installStrictPositionEnforcer } from "./strictPositionEnforcer.js?v=strict-cdm-1";
 import { installSeasonStartBridge } from "./seasonStartBridge.js?v=live-balanced-engine-1";
