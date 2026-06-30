ae09bf718c1023d1242be87072d1519c373c45a3
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-06-30 15:48:27 +0200

    Use complete chain UI enhancer

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -3,7 +3,7 @@ import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js?v=default-fast-green-draft-1";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
 import { installFormationSelectorEnhancer } from "./formationSelectorEnhancer.js?v=formation-links-1";
-import { installTraitChainEnhancer } from "./traitChainEnhancer.js?v=balanced-trait-recipes-1";
+import { installTraitChainEnhancer } from "./traitChainEnhancerComplete.js?v=complete-chain-ui-1";
 import { installTacticsSystemEnhancer } from "./tacticsSystemEnhancer.js";
 import { installStrictPositionEnforcer } from "./strictPositionEnforcer.js?v=strict-cdm-1";
 import { installSeasonStartBridge } from "./seasonStartBridge.js?v=live-balanced-engine-1";
