c950fbba33c942e0f44188cf1e5863424aa13b99
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-07-01 15:06:03 +0200

    Bust app cache for start page redesign

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -1,4 +1,4 @@
-import { goTo } from "./linearRouter.js?v=english-ui-1";
+import { goTo } from "./linearRouter.js?v=start-bookmaker-1";
 import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js?v=default-fast-green-draft-1";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=draft-speed-live-3";
