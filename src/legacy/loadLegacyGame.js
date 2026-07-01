bf8fe5d80353328cacc71ecfde92b7a548a0d498
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-07-01 14:17:04 +0200

    Remove legacy loader file

--- src/legacy/loadLegacyGame.js
+++
@@ -1,9 +0,0 @@
-export async function loadLegacyGame() {
-  const module = await import("../../js/linear/linearApp.js?v=react-shell-2");
-
-  if (typeof module.bootLinearApp !== "function") {
-    throw new Error("Legacy game module does not export bootLinearApp().");
-  }
-
-  return module.bootLinearApp;
-}
