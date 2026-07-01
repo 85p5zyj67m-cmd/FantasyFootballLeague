9bd32d371e7a0ec11352407e5973a77375db6132
85p5zyj67m-cmd <85p5zyj67m@privaterelay.appleid.com>
2026-07-01 17:15:47 +0200

    Refresh draft constraint enhancer cache

--- js/linear/linearApp.js
+++ js/linear/linearApp.js
@@ -3,7 +3,7 @@ import { installLinearStyles } from "./linearStyles.js";
 import { installDraftPolishStyles } from "./draftPolishStyles.js?v=default-fast-green-draft-1";
 import { installDraftSpeedController } from "./draftSpeedController.js?v=instant-draft-batch-1";
 import { installFormationSelectorEnhancer } from "./formationSelectorEnhancer.js?v=formation-links-1";
-import { installDraftFormationConstraintEnhancer } from "./draftFormationConstraintEnhancerV6.js?v=draft-constraint-perf-1";
+import { installDraftFormationConstraintEnhancer } from "./draftFormationConstraintEnhancerV6.js?v=draft-status-hidden-1";
 import { installTraitChainEnhancer } from "./traitChainEnhancerCompleteV6.js?v=show-all-control-robust-1";
 import { installMatchActionTopEnhancer } from "./matchActionTopEnhancer.js?v=page-flow-polish-1";
 import { installLiveGoalScorersEnhancer } from "./liveGoalScorersEnhancer.js?v=goal-ball-only-1";
